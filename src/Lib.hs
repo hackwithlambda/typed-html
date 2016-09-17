777c5b5b023aff2cb6358998f68df304feff2dc2
Bryan Garza <brygarza@gmail.com>
2016-09-17 13:25:46 -0700

    Initial commit

---
+++ src/Lib.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE DataKinds #-}
+
+module Lib
+    (
+    ) where
+
+data Tree Tag a
+  = Leaf tag a
+  | Branch tag a [Tree a]
+
+type Html = Tree Tag
+
+newtype Attr = Attr { getAttr :: (Property, Value) }
+
+data Property = Src
+  | Href
+
+data Tag = Head
+  | P
+  | I
+  | Strong
+  | Small
